7a5ce0044e0264283a893ef7c84cbe36dcfa8535
VKGroup1 <155010512+VKGroup1@users.noreply.github.com>
2025-02-02 09:19:39 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const container = document.querySelector('.container');
-    container.addEventListener("click", function() {
-        alert("Happy Rose Day! JihoonInc_ loves Chrish ❤️");
-    });
-});
