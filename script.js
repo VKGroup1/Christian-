57e8e159f44103c5a04edd7aefc62db8061e46d5
VKGroup1 <155010512+VKGroup1@users.noreply.github.com>
2025-02-02 09:21:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const container = document.querySelector('.container');
+    container.addEventListener("click", function() {
+        alert("Happy Valentine's Day! JihoonInc_ loves Chrish ❤️");
+    });
+});
